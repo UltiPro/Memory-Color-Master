b147fe95d9e059f2bdd3c52d877daaa73789e509
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2024-01-31 01:09:53 +0100

    Update

--- Memory Color Master/index.js
+++ Memory Color Master/index.js
@@ -1,12 +1,25 @@
 $(function () {
-    const template = $("#game-box-item-template").html();
+    let randomColors = [];
+    for (let i = 0; i < 15; i++) randomColors[i] = getRandomColor();
 
+    const template = $("#game-box-item-template").html();
     for (let i = 0; i < 30; i++) $(".game-box").append(template);
 
     const all = $(".game-box-item");
+    let numbers = [...Array(30).keys()];
+    let firstNum, secondNum;
+    
+    for (let i = 0; i < (all.length / 2); i++) {
+        firstNum = numbers[Math.floor(Math.random() * numbers.length)];
+        numbers.splice(numbers.indexOf(firstNum), 1);
+        secondNum = numbers[Math.floor(Math.random() * numbers.length)];
+        numbers.splice(numbers.indexOf(secondNum), 1);
+
+        $(".game-box-item").eq(firstNum).children().children().eq(1).css("background", randomColors[i]);
+        $(".game-box-item").eq(secondNum).children().children().eq(1).css("background", randomColors[i]);
+    }
 
     $("#reset-button").on("click", function (e) {
-
         const circle = document.createElement('span');
         circle.classList.add('circle');
         circle.style.top = (e.clientY - e.delegateTarget.offsetParent.offsetTop) + 'px';
@@ -23,3 +36,10 @@ $(function () {
 
     });
 });
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) color += letters[Math.floor(Math.random() * 16)];
+    return color;
+}
