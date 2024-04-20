f69b20ea5868ce0218752a7b10f246b79fb311f7
Patryk Wójtowicz <ultiprogames@gmail.com>
2024-04-20 20:46:22 +0200

    Update index.js

--- Memory Color Master/index.js
+++ Memory Color Master/index.js
@@ -1,79 +1,70 @@
-$(function () {
+$(() => {
+    const letters = "0123456789ABCDEF";
+
     function getRandomColor() {
-        const letters = '0123456789ABCDEF';
-        let color = '#';
+        let color = "#";
         for (let i = 0; i < 6; i++) color += letters[Math.floor(Math.random() * 16)];
         return color;
     }
 
-    function handleGame() {
-        if (first.attr("number").substring(1) == second.attr("number").substring(1)) {
-            first.prop("onclick", null).off("click");
-            second.prop("onclick", null).off("click");
+    function handleGame(secondSquare) {
+        if (square.attr("number").substring(1) == secondSquare.attr("number").substring(1)) {
+            square.off("click");
+            square.children().css("cursor", "default");
+            square.animate({ opacity: "0" });
 
-            first.children().css("cursor", "default");
-            second.children().css("cursor", "default");
-
-            first.animate({ opacity: "0" });
-            second.animate({ opacity: "0" });
+            secondSquare.off("click");
+            secondSquare.children().css("cursor", "default");
+            secondSquare.animate({ opacity: "0" });
 
             squaresNum--;
-            if (!squaresNum) return false;
+            if (!squaresNum) return true;
         }
         else {
-            first.children().css("transform", "rotateY(0deg)");
-            second.children().css("transform", "rotateY(0deg)");
+            square.children().css("transform", "rotateY(0)");
+            secondSquare.children().css("transform", "rotateY(0)");
         }
-        return true;
+        return false;
     }
-
-    let squaresNum = 15;
-
-    let randomColors = [];
-    for (let i = 0; i < squaresNum; i++) randomColors[i] = getRandomColor();
 
     const template = $("#game-box-item-template").html();
-    for (let i = 0; i < squaresNum * 2; i++) $(".game-box").append(template);
-
-    let numbers = [...Array(squaresNum * 2).keys()];
-    let first, second;
+    const randomColors = [];
+    let squaresNum = 15;
 
     for (let i = 0; i < squaresNum; i++) {
-        first = numbers[Math.floor(Math.random() * numbers.length)];
-        numbers.splice(numbers.indexOf(first), 1);
-
-        second = numbers[Math.floor(Math.random() * numbers.length)];
-        numbers.splice(numbers.indexOf(second), 1);
-
-        $(".game-box-item").eq(first).attr("number", "f" + i);
-        $(".game-box-item").eq(first).children().children().eq(1).css("background", randomColors[i]);
-        $(".game-box-item").eq(second).attr("number", "s" + i);
-        $(".game-box-item").eq(second).children().children().eq(1).css("background", randomColors[i]);
+        randomColors[i] = getRandomColor();
+        $(".game-box").append(template);
+        $(".game-box").append(template);
     }
 
-    first = second = null;
-    blockGame = false;
-    isGameOn = true;
+    const numbers = [...Array(squaresNum * 2).keys()];
+    let square;
 
-    $(".game-box-item").on("click", async function () {
-        if (blockGame) return;
-
-        if (first == null) {
-            first = $(this);
-            first.children().css("transform", "rotateY(180deg)");
+    for (let i = 0; i < squaresNum; i++) {
+        for (let j = 0; j < 2; j++) {
+            square = numbers[Math.floor(Math.random() * numbers.length)];
+            numbers.splice(numbers.indexOf(square), 1);
+            $(".game-box-item").eq(square).attr("number", (j == 0 ? "f" : "s") + i);
+            $(".game-box-item").eq(square).children().children().eq(1).css("background", randomColors[i]);
         }
-        else if (first.attr("number") != $(this).attr("number")) {
-            blockGame = true;
+    }
 
-            second = $(this);
-            second.children().css("transform", "rotateY(180deg)");
+    square = null;
+    blockGame = isGameOver = false;
+
+    $(".game-box-item").on("click", function () {
+        if (blockGame) return;
+        if (square == null) {
+            square = $(this);
+            square.children().css("transform", "rotateY(180deg)");
+        }
+        else if (square.attr("number") != $(this).attr("number")) {
+            blockGame = true;
+            $(this).children().css("transform", "rotateY(180deg)");
 
             setTimeout(() => {
-                isGameOn = handleGame();
-
-                first = null;
-                second = null;
-
+                isGameOver = handleGame($(this));
+                square = null;
                 blockGame = false;
             }, 750);
         }
@@ -82,26 +73,24 @@ $(function () {
     const startTime = new Date;
     let time;
 
-    let timer = setInterval(() => {
+    const timer = setInterval(() => {
         time = new Date(new Date - startTime);
 
         $("#score").text((time.getMinutes() < 10 ? "0" + time.getMinutes() : getMinutes()) +
             ":" + (time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds()));
 
-        if (!isGameOn) {
+        if (isGameOver) {
             clearInterval(timer);
             $(".game-box").empty();
-            $(".game-box").append("<div class='game-box-you-won'>YOU WON</div>")
+            $(".game-box").append("<div class='game-box-you-won'>YOU WON</div>");
         }
     }, 1000);
 
-    $("#reset-button").on("click", function (e) {
-        const circle = document.createElement('span');
-
-        circle.classList.add('circle');
-
-        circle.style.top = (e.clientY - e.delegateTarget.offsetParent.offsetTop) + 'px';
-        circle.style.left = (e.clientX - e.delegateTarget.offsetParent.offsetLeft) + 'px';
+    $("#restart").on("click", function (e) {
+        const circle = document.createElement("span");
+        circle.classList.add("circle");
+        circle.style.top = (e.clientY - e.delegateTarget.offsetParent.offsetTop) + "px";
+        circle.style.left = (e.clientX - e.delegateTarget.offsetParent.offsetLeft) + "px";
 
         e.target.appendChild(circle);
 
